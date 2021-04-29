afcd14aef7fd248539157a6373925af8855371d2
Mhayk Whandson <hi@mhayk.com>
2021-04-29 11:17:34 +0100

    The component Sidebar has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
+import { Flex } from "@chakra-ui/layout";
+import { SideBar } from "../components/Form/Sidebar";
 import { Header } from "../components/Header";
 
 export default function Dashboard() {
     return (
-        <Header />
+        <Flex direction="column" h="100vh">
+            <Header />
+
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                <SideBar />
+            </Flex>
+        </Flex>
     )
 }
