11e924295756525ce9e77fdc417c7f303ddf605e
Mhayk Whandson <hi@mhayk.com>
2021-05-05 16:53:59 +0100

    User list page has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,121 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import React from "react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { SideBar } from "../../components/Form/Sidebar";
+import { Header } from "../../components/Header";
+
+export default function UserList() {
+    return (
+        <Box>
+            <Header />
+
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                <SideBar />
+
+                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                    <Flex mb="8" justify="space-between" align="center">
+                        <Heading size="lg" fontWeight="normal">Users</Heading>
+
+                        <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="pink"
+                            leftIcon={<Icon as={RiAddLine}
+                                fontSize="20"
+                            />}
+                        >
+                            Create a new user
+                        </Button>
+                    </Flex>
+
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
+                                <Th>User</Th>
+                                <Th>Registration Date</Th>
+                                <Th width="8"></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Mhayk Whandson</Text>
+                                        <Text fontSize="sm" color="gray.300">hi@mhayk.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 April, 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Edit
+                                    </Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Mhayk Whandson</Text>
+                                        <Text fontSize="sm" color="gray.300">hi@mhayk.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 April, 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Edit
+                                    </Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Mhayk Whandson</Text>
+                                        <Text fontSize="sm" color="gray.300">hi@mhayk.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 April, 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Edit
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </Box>
+
+
+            </Flex>
+        </Box >
+    );
+}
