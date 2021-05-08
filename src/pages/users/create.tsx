7a3aeb84f7839834af676b31de983f2f496918a8
Mhayk Whandson <hi@mhayk.com>
2021-05-08 18:56:13 +0100

    The page to create a new user has adjusted to be responsive.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -12,18 +12,18 @@ export default function CreateUser() {
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <SideBar />
 
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
                     <Heading size="lg" fontWeight="normal">Create a user</Heading>
 
                     <Divider my="6" borderColor="gray.700" />
 
                     <VStack spacing="8">
-                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                        <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
                             <Input name="name" label="Full name" />
                             <Input name="email" type="email" label="E-mail" />
                         </SimpleGrid>
 
-                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                        <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
                             <Input name="password" type="password" label="Password" />
                             <Input name="password_confirmation" type="password" label="Password confirmation" />
                         </SimpleGrid>
