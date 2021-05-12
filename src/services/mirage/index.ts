39cdf63b8ac8fcfb7153cf2daaabc1e74d381285
Mhayk Whandson <hi@mhayk.com>
2021-05-12 20:05:54 +0100

    The factory to create 200 random users has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,5 @@
-import { createServer, Model } from 'miragejs'
+import { createServer, Factory, Model } from 'miragejs'
+import faker from 'faker'
 
 type User = {
     name: string;
@@ -10,6 +11,24 @@ export function makeServer() {
     const server = createServer({
         models: {
             user: Model.extend<Partial<User>>({})
+        },
+
+        factories: {
+            user: Factory.extend({
+                name(i: number) {
+                    return `User ${i + 1}`
+                },
+                email() {
+                    return faker.internet.email().toLowerCase()
+                },
+                createdAt() {
+                    return faker.date.recent(10)
+                },
+            })
+        },
+
+        seeds(server) {
+            server.createList('user', 200)
         },
 
         routes() {
