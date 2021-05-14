9d7970fa0ad9e29620113dac446c87d385cdb300
Mhayk Whandson <hi@mhayk.com>
2021-05-14 19:43:51 +0100

    Whe the state is "IsFetching", a small spinner is showed.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link'
 import { useQuery } from 'react-query'
 
 export default function UserList() {
-    const { data, isLoading, error } = useQuery('users', async () => {
+    const { data, isLoading, isFetching, error } = useQuery('users', async () => {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
 
@@ -27,6 +27,8 @@ export default function UserList() {
         })
 
         return users
+    }, {
+        staleTime: 1000 * 5
     })
 
     const isWideVersion = useBreakpointValue({
@@ -44,7 +46,11 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Users</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Users
+
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+                        </Heading>
 
                         <Link href="/users/create" passHref>
                             <Button
