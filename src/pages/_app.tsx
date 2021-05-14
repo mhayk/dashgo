518de19d0ebc35e494711c114bd5de01cfa7e5a2
Mhayk Whandson <hi@mhayk.com>
2021-05-14 16:34:32 +0100

    React query provider has been added in the project.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,23 @@ import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../Contexts/SidebarDrawerContext'
 import { makeServer } from '../services/mirage'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 if (process.env.NODE_ENV === "development") {
   makeServer()
 }
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
