8187db3aa34d8a8190699017d8cf14a17c2e9d67
Mhayk Whandson <hi@mhayk.com>
2021-05-14 19:09:46 +0100

    The data from '/users' are being showed now.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,7 +13,20 @@ export default function UserList() {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
 
-        return data
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('en-GB', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        })
+
+        return users
     })
 
     const isWideVersion = useBreakpointValue({
@@ -70,75 +83,33 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Mhayk Whandson</Text>
-                                                <Text fontSize="sm" color="gray.300">hi@mhayk.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>04 April, 2021</Td>}
-                                        <Td>
-                                            <Button
-                                                as="a"
-                                                size="sm"
-                                                fontSize="sm"
-                                                colorScheme="purple"
-                                                leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                            >
-                                                Edit
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>{user.createdAt}</Td>}
+                                                <Td>
+                                                    <Button
+                                                        as="a"
+                                                        size="sm"
+                                                        fontSize="sm"
+                                                        colorScheme="purple"
+                                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                                    >
+                                                        Edit
                                     </Button>
-                                        </Td>
-                                    </Tr>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Mhayk Whandson</Text>
-                                                <Text fontSize="sm" color="gray.300">hi@mhayk.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>04 April, 2021</Td>}
-                                        <Td>
-                                            <Button
-                                                as="a"
-                                                size="sm"
-                                                fontSize="sm"
-                                                colorScheme="purple"
-                                                leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                            >
-                                                Edit
-                                    </Button>
-                                        </Td>
-                                    </Tr>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Mhayk Whandson</Text>
-                                                <Text fontSize="sm" color="gray.300">hi@mhayk.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>04 April, 2021</Td>}
-                                        <Td>
-                                            <Button
-                                                as="a"
-                                                size="sm"
-                                                fontSize="sm"
-                                                colorScheme="purple"
-                                                leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                            >
-                                                Edit
-                                    </Button>
-                                        </Td>
-                                    </Tr>
+                                                </Td>
+                                            </Tr>
+                                        )
+                                    })}
                                 </Tbody>
                             </Table >
 
