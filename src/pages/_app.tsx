9fcb0715229b4f4000eeaf400ee864a7971c5695
Mhayk Whandson <hi@mhayk.com>
2021-05-17 18:13:54 +0100

    queryClient has been imported from the queryClient service.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,12 @@ import { SidebarDrawerProvider } from '../Contexts/SidebarDrawerContext'
 import { makeServer } from '../services/mirage'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from '../services/queryClient'
 
 if (process.env.NODE_ENV === "development") {
   makeServer()
 }
-
-const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
