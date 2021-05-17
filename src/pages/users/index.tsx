b3df2861f83839e170b91d9b80bc7c35e6e39c87
Mhayk Whandson <hi@mhayk.com>
2021-05-17 18:18:23 +0100

    the user type has been changed to string
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -19,7 +19,7 @@ export default function UserList() {
         lg: true
     })
 
-    async function handlePrefetchUser(userId: number) {
+    async function handlePrefetchUser(userId: string) {
         await queryClient.prefetchQuery(['user', userId], async () => {
             const response = await api.get(`users/${userId}`)
 
@@ -89,7 +89,7 @@ export default function UserList() {
                                                 </Td>
                                                 <Td>
                                                     <Box>
-                                                        <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(Number(user.id))}>
+                                                        <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
                                                             <Text fontWeight="bold">{user.name}</Text>
                                                         </Link>
                                                         <Text fontSize="sm" color="gray.300">{user.email}</Text>
