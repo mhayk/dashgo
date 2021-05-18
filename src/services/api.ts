b602acade47495ac557612b099df4d04b7c7bee0
Mhayk Whandson <hi@mhayk.com>
2021-05-18 15:27:17 +0100

    The authorization value is filled in the header of all requests via api service.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,11 @@
 import axios from 'axios'
+import { parseCookies } from 'nookies'
+
+const cookies = parseCookies();
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'http://localhost:3000/api',
+    headers: {
+        Authorization: `Bearer ${cookies['nextauth.token']}`
+    }
 })
