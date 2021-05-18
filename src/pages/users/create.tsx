de408038b4269bf12905989b5918769240e64450
Mhayk Whandson <hi@mhayk.com>
2021-05-18 10:05:53 +0100

    useMutation has been use to do a post request to create a new user.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -7,6 +7,10 @@ import { SideBar } from "../../components/Sidebar";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Form/Input";
 import Link from "next/link";
+import { useMutation } from 'react-query'
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
     name: string;
@@ -25,14 +29,31 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
+    const router = useRouter()
+
+    const createUser = useMutation(async (user: CreateUserFormData) => {
+        const response = await api.post('users', {
+            user: {
+                ...user,
+                created_at: new Date()
+            }
+        })
+
+        return response.data.user;
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('users')
+        }
+    })
+
     const { register, handleSubmit, formState } = useForm({
         resolver: yupResolver(createUserFormSchema)
     })
 
     const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-        await new Promise(resolve => setTimeout(resolve, 2000))
+        await createUser.mutateAsync(values)
 
-        console.log(values)
+        router.push('/users')
     }
 
     return (
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from 'miragejs'
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from 'miragejs'
 import faker from 'faker'
 
 type User = {
@@ -9,6 +9,10 @@ type User = {
 
 export function makeServer() {
     const server = createServer({
+        // serializers: {
+        //     application: ActiveModelSerializer
+        // },
+
         models: {
             user: Model.extend<Partial<User>>({})
         },
@@ -28,7 +32,7 @@ export function makeServer() {
         },
 
         seeds(server) {
-            server.createList('user', 200)
+            server.createList('user', 30)
         },
 
         routes() {
