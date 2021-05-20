347fa2323838b0dea2b0884a43c52b5f0f8c63f4
Mhayk Whandson <hi@mhayk.com>
2021-05-20 17:12:02 +0100

    Catch has been added and console.log has been removed.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -19,8 +19,9 @@ export default function Dashboard() {
             baseURL: 'http://localhost:3333'
         })
             .then(response => console.log(response))
+            .catch(err => console.log(err))
     })
-    console.log(user)
+
     const options = {
         chart: {
             toolbar: {
