4ef47c58cc91b6582f269a79e62e1a06e592e1dd
Mhayk Whandson <hi@mhayk.com>
2021-05-21 19:29:07 +0100

    The code has been decoupled then a new function named validateUserPermissions has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ src/utils/validadeUserPermissions.ts
@@ -0,0 +1,39 @@
+type User = {
+    permissions: string[];
+    roles: string[];
+}
+
+type validateUserPermissionsParams = {
+    user: User;
+    permissions?: string[];
+    roles?: string[];
+}
+
+export function validateUserPermissions({
+    user,
+    permissions,
+    roles }: validateUserPermissionsParams) {
+
+    if (permissions?.length > 0) {
+        const hasAllPermissions = permissions.every(permission => {
+            return user.permissions.includes(permission)
+        })
+
+        if (!hasAllPermissions) {
+            return false;
+        }
+    }
+
+    if (roles?.length > 0) {
+        const hasAllRoles = roles.some(role => {
+            return user.roles.includes(role)
+        })
+
+        if (!hasAllRoles) {
+            return false;
+        }
+    }
+
+    return true
+
+}
